83815c1eb11063e8e8e32de7d6cee62311888682
Alben <albensnico@gmail.com>
2022-04-20 16:26:29 -0500

    User entity defined

---
+++ src/main/java/com/alnicode/mycontactlist/entity/User.java
@@ -0,0 +1,33 @@
+package com.alnicode.mycontactlist.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @NotNull
+    @Size(min = 5)
+    private String username;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    @Column(name = "role_name")
+    private String role;
+}
