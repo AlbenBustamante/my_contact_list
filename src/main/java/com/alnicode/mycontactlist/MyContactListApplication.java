5d280c20b9517987c6eaaff281b2f08e6e9ce191
Alben <albensnico@gmail.com>
2022-04-20 02:19:24 -0500

    Initial commit

---
+++ src/main/java/com/alnicode/mycontactlist/MyContactListApplication.java
@@ -0,0 +1,13 @@
+package com.alnicode.mycontactlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyContactListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyContactListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alnicode/mycontactlist/MyContactListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alnicode.mycontactlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyContactListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
