0deb798fc831fdd975435f4553e4991a90c26a65
Alben <albensnico@gmail.com>
2022-04-20 02:55:36 -0500

    Contact dtos defined

---
+++ src/main/java/com/alnicode/mycontactlist/dto/contact/ContactRequest.java
@@ -0,0 +1,29 @@
+package com.alnicode.mycontactlist.dto.contact;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Data;
+
+@Data
+public class ContactRequest {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String lastname;
+
+    @NotBlank
+    private String phone;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String direction;
+
+    @Min(1L)
+    private long bookId;
+}
